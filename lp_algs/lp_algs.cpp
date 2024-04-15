21b76b2a758b629bfaad0728338d2834f53d293e
nol0n <sharapovegor2003@gmail.com>
2024-04-15 13:29:36 +0300

    simplex method refactoring

--- lp_algs/lp_algs.cpp
+++ lp_algs/lp_algs.cpp
@@ -7,6 +7,39 @@
 #include <rational.hpp>
 #include <table.hpp>
 
+namespace simplexMethod {
+
+    void findPositiveValueInRow(const nol0n::Table& table, const size_t& columns, size_t& column)
+    {
+        for (int j = 1; j < columns; ++j)
+        {
+            // если нашли положительный коэффициент, то запоминаем индекс 
+            // его столбца, иначе индекс будет равен -1
+            if (table(0, j) > nol0n::rational(0))
+            {
+                column = j;
+                break;
+            }
+        }
+    }
+
+    void findMinmumRelationInColumn(const nol0n::Table& table, const size_t& rows, const size_t& column, size_t& row)
+    {
+        // найдем элемент в столбце, который будет иметь отношени, при этом сам будет
+        // отрицательным если все элементы положительные, то row будет = -1
+        nol0n::rational tmp = 0;
+        for (size_t i = 1; i < rows; ++i)
+        {
+            if (table(i, column) < nol0n::rational(0) && (table(i, 0) / table(i, column)) > tmp)
+            {
+                row = i;
+                tmp = table(i, 0) / table(i, column);
+            }
+        }
+    }
+
+} // namespace anonymous
+
 namespace nol0n
 {
     /// @brief find optimal solution (non-integer) for column table
@@ -24,19 +57,8 @@ namespace nol0n
 
         while (true)
         {
-            // проходим по первой строке в поиске
-            // положительных значений
-            for (int j = 1; j < columns; ++j)
-            {
-                // если нашли положительный коэффициент, то
-                // запоминаем индекс его столбца, иначе индекс
-                // будет равен -1
-                if (table(0, j) > rational(0))
-                {
-                    column = j;
-                    break;
-                }
-            }
+            // проходим по первой строке в поиске положительных значений
+            simplexMethod::findPositiveValueInRow(table, columns, column);
 
             // не было найдено положительного значения
             // в первом столбце => мы нашли оптимальное решение
@@ -45,30 +67,18 @@ namespace nol0n
                 return 1;
             }
 
-            // если мы нашли отрицательное значение, то необхожимо
-            // преобразовать таблицу
+            // если мы нашли отрицательное значение, то необхожимо преобразовать таблицу
 
-            // найдем элемент в столбце, который будет иметь
-            // отношение min(abs(a)), при этом сам будет отрицательным
-            // если все элементы положительные, то оптимального плана нет
-            rational tmp = 0;
-            for (size_t i = 1; i < rows; ++i)
-            {
-                if (table(i, column) < rational(0) && (table(i, 0) / table(i, column)) > tmp)
-                {
-                    row = i;
-                    tmp = table(i, 0) / table(i, column);
-                }
-            }
+            // найдем элемент в столбце, который будет иметь отношени, при этом сам будет
+            // отрицательным если все элементы положительные, то оптимального плана нет
+            simplexMethod::findMinmumRelationInColumn(table, rows, column, row);
 
-            // не удалось найти допустимый элемент
-            // оптимального решения не сущесвтует
+            // не удалось найти допустимый элемент оптимального решения не сущесвтует
             if (row == -1)
             {
                 return 0;
             }
 
-            // зануляем выбранную строку
             table.rowZeroing(row, column);
             if (debug)
                 std::cout << table << "\n\n";
